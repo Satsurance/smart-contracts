855cee2197dfad21c1602641f7d4a59afa1416a9
Nick Ivanov <nick@MacBook-Air-Nick.local>
2024-12-05 21:38:11 +0100

    project start

---
+++ contracts/SursToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract SursToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor(
+        uint256 initialSupply
+    ) ERC20("Satsurance", "SURC") ERC20Permit("Satsurance") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, value);
+    }
+
+    function nonces(
+        address owner
+    ) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+}
