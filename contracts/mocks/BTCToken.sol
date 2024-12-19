9ba90ad52d8a2d5ea2b1ec8a83da5e3ec2f2ba9b
Nick Ivanov <nick@MacBook-Air-Nick.local>
2024-12-19 20:16:01 +0100

    update mock token

--- contracts/mocks/BTCToken.sol
+++ contracts/mocks/BTCToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BTCToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Satsurance", "SURC") {
+    constructor(uint256 initialSupply) ERC20("WrappedBTC", "WBTC") {
         _mint(msg.sender, initialSupply);
     }
 }
