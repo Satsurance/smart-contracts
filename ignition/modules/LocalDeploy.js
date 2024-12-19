1064a40c5be42b41f191445a5490cd709df64719
Nick Ivanov <nick@MacBook-Air-Nick.local>
2024-12-19 20:36:17 +0100

    add local deploy ignition

--- ignition/modules/LocalDeploy.js
+++ ignition/modules/LocalDeploy.js
@@ -4,7 +4,8 @@ const { InsuranceSetup } = require("./Insurance");
 
 const BIG_STAKER_ADDR = "0xe63b611C72e046e5FA05C3EaA972F2bbD6E9a1CB";
 const POOL_OWNER_ADDR = "0xe1a5328f489C261410563a08a92f1FFdfF045407";
-const TokenOwnerModule = buildModule("TokenOwnerModule", async (m) => {
+
+module.exports = buildModule("LocalDeploy", async (m) => {
   const { btcToken, sursToken, insurancePool, governor_c, timelock } =
     m.useModule(InsuranceSetup);
 
